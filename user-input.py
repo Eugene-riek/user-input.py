1791c52f037cf54932f64c4655d9d34043caf81a
Eugene Israel <81667755+Eugene-riek@users.noreply.github.com>
2024-02-22 15:09:58 +0300

    Create user-input.py
    This my first python assignment

---
+++ user-input.py
@@ -0,0 +1,4 @@
+name = input("What is your name?")
+age = input("How old are you?")
+location = input("Where do you come from?")
+print("Hello {}, you are {}, and you come from {}.".format(name, age, location))
